44a8e316430cb3ea4dc0144499b68fcd0a77c5e1
gutao <2437987361@qq.com>
2014-12-23 13:32:59 +0800

    register

--- src/com/pshow/goldpush/MainActivity.java
+++ src/com/pshow/goldpush/MainActivity.java
@@ -11,6 +11,8 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.protocol.HTTP;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.conn.params.ConnManagerParams;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -20,6 +22,7 @@ import java.net.URL;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,12 +37,19 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.widget.EditText;
 
+
 public class MainActivity extends Activity {
 	String strcon;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+	        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+	        StrictMode.setThreadPolicy(policy);
+	    }
+		
 		PlaceholderFragment bFrag  = new PlaceholderFragment(); 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
@@ -109,35 +119,67 @@ public class MainActivity extends Activity {
 					        		strUrl += mytext.getText();
 					        		strUrl += "&deviceType=android";
 					        		
-					        		URL url = null;
-					        		try{
-					        			String result="";
-					                    
-					                    HttpGet httpGet=new HttpGet(strUrl);
-					                    //取得HTTP response
-					                    HttpResponse response=new DefaultHttpClient().execute(httpGet);
-					                    //若状态码为200
-					                    if(response.getStatusLine().getStatusCode()==200){
-					                            //取出应答字符串
-					                            HttpEntity entity=response.getEntity();
-					                            result=EntityUtils.toString(entity, HTTP.UTF_8);
-					                    }
-					        			
-//					        			new AlertDialog.Builder(getActivity()) 
-//						                .setMessage(result)  
-//						                .setPositiveButton("确定",  
-//						                        new DialogInterface.OnClickListener() {  
-//						                            @Override  
-//						                            public void onClick(DialogInterface dialog,  
-//						                                    int which) {  
-//						                                // TODO Auto-generated method stub  
-//						  
-//						                            }  
-//						                        }).setNegativeButton("取消", null).create()  
-//						                .show();
-					        		}catch(Exception e){
-					        			e.printStackTrace();
-					        		}
+					        		HttpClient httpClient = new DefaultHttpClient(); // 新建HttpClient对象  
+					                HttpConnectionParams  
+					                        .setConnectionTimeout(httpClient.getParams(), 10000); // 设置连接超时  
+					                HttpConnectionParams.setSoTimeout(httpClient.getParams(), 10000); // 设置数据读取时间超时  
+					                ConnManagerParams.setTimeout(httpClient.getParams(), 10000); // 设置从连接池中取连接超时  
+					          
+					                HttpGet httpget = new HttpGet(strUrl); // 获取请求  
+					          
+					                try {  
+					                    HttpResponse response = httpClient.execute(httpget); // 执行请求，获取响应结果  
+					                    if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) { // 响应通过  
+					                        String result = EntityUtils.toString(response.getEntity(),  
+					                                "UTF-8");  
+					                        if(result.equals("success")){
+					                        	new AlertDialog.Builder(getActivity()) 
+								                .setMessage("注册成功")  
+								                .setPositiveButton("确定",  
+								                        new DialogInterface.OnClickListener() {  
+								                            @Override  
+								                            public void onClick(DialogInterface dialog,  
+								                                    int which) {  
+								                                // TODO Auto-generated method stub  
+								  
+								                            }  
+								                        }).setNegativeButton("取消", null).create()  
+								                .show();
+					                        }else{
+					                        	new AlertDialog.Builder(getActivity()) 
+								                .setMessage("注册失败")  
+								                .setPositiveButton("确定",  
+								                        new DialogInterface.OnClickListener() {  
+								                            @Override  
+								                            public void onClick(DialogInterface dialog,  
+								                                    int which) {  
+								                                // TODO Auto-generated method stub  
+								  
+								                            }  
+								                        }).setNegativeButton("取消", null).create()  
+								                .show();
+					                        }
+					                    } else {  
+					                    	new AlertDialog.Builder(getActivity()) 
+							                .setMessage("注册失败")  
+							                .setPositiveButton("确定",  
+							                        new DialogInterface.OnClickListener() {  
+							                            @Override  
+							                            public void onClick(DialogInterface dialog,  
+							                                    int which) {  
+							                                // TODO Auto-generated method stub  
+							  
+							                            }  
+							                        }).setNegativeButton("取消", null).create()  
+							                .show();  
+					                    }  
+					                } catch (ClientProtocolException e) {  
+					                    // TODO Auto-generated catch block  
+					                    e.printStackTrace();  
+					                } catch (IOException e) {  
+					                    // TODO: handle exception  
+					                    e.printStackTrace();  
+					                } 
 				        		}
 				        		else{
 					        		new AlertDialog.Builder(getActivity()) 
